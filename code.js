ba9f85514029d62e811a27dffaf09d23bc9b7c5b
khanh_peter <98100088+konoha-minato@users.noreply.github.com>
2023-04-10 21:34:47 +0700

    commit

--- code.js
+++ code.js
@@ -273,7 +273,7 @@ function add_tn(){
   ar_cp.push(document.getElementById("tth_r").textContent)
   row.innerHTML=
   '<tr>'+
-    '<th rowspan="2">'+ar_cp[0]+'</th>'+
+    '<th rowspan="2" style=" border-style: groove; border-width: 2px;border-color: brown;" >'+ar_cp[0]+'</th>'+
     '<th colspan="3">'+ten_thuoc+" | "+[ar_cp[1],ar_cp[2],ar_cp[3]].join("_")+" | " +ar_cp[7]+"%"+'</th>'+
   '</tr>'
   row = table.insertRow(-1)
